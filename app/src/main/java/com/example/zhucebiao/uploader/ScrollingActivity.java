75f2ceec1895b655f20af32e0bbc65037b9d77eb
zhucebiao <sshhsh@foxmail.com>
2017-08-17 01:22:04 +0800

    Upkoadind and overwrite single file

--- app/src/main/java/com/example/zhucebiao/uploader/ScrollingActivity.java
+++ app/src/main/java/com/example/zhucebiao/uploader/ScrollingActivity.java
@@ -14,11 +14,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+
+import jcifs.smb.SmbException;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileOutputStream;
 
 public class ScrollingActivity extends AppCompatActivity {
     EditText text_name;
     EditText text_pass;
+    EditText text_server;
+    File file;
+    SmbFile smbFile;
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,73 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String server = text_server.getText().toString().trim();
+                        String name = text_name.getText().toString().trim();
+                        String pass = text_pass.getText().toString();
+
+                        SharedPreferences.Editor editor = getSharedPreferences("lock", MODE_PRIVATE).edit();
+                        editor.putString("server", server);
+                        editor.putString("name", name);
+                        editor.putString("pass", pass);
+                        editor.apply();
+
+                        if (flag) try {
+                            String smbUrl = "smb://" + name + ":" + pass + "@" + server + "/temp/filesFromUploader/" + file.getName();
+                            Log.e("uri", smbUrl);
+                            FileInputStream in = new FileInputStream(file);
+                            smbFile = new SmbFile(smbUrl);
+                            smbFile.connect();
+                            if (!smbFile.exists())
+                                smbFile.createNewFile();
+                            SmbFileOutputStream out = new SmbFileOutputStream(smbFile);
+                            OutputStream fout = new BufferedOutputStream(out);
+                            byte b[] = new byte[5 * 1024];
+                            int len;
+                            while ((len = in.read(b)) != -1) {
+                                fout.write(b, 0, len);
+                            }
+                            fout.flush();
+                            fout.close();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                            showMessage("FileNotFoundException");
+                        } catch (UnknownHostException e) {
+                            e.printStackTrace();
+                            showMessage("UnknownHostException");
+                        } catch (SmbException e) {
+                            e.printStackTrace();
+                            showMessage("SmbException");
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                            showMessage("MalformedURLException");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            showMessage("IOException");
+                        }
+                    }
+                }).start();
+
+
+            }
+        });
+
+        text_server = (EditText) findViewById(R.id.editText_server);
+        text_name = (EditText) findViewById(R.id.editText_name);
+        text_pass = (EditText) findViewById(R.id.editText_pass);
+
+        SharedPreferences read = getSharedPreferences("lock", MODE_PRIVATE);
+        text_server.setText(read.getString("server", "Server address"));
+        text_name.setText(read.getString("name", "User Name"));
+        text_pass.setText(read.getString("pass", "Password"));
+
         Intent intent = getIntent();
         String action = getIntent().getAction();
         Log.e("action", action);
@@ -33,34 +115,16 @@ public class ScrollingActivity extends AppCompatActivity {
             Uri uri = intent.getData();
             if (uri != null) {
                 Log.e("uri", uri.getPath());
-                File file = new File(uri.getPath());
-                if (file.exists()) {
+                file = new File(uri.getPath());
+                if (file.isFile() && file.exists()) {
                     Log.e("uri", "exist");
-
-                } else Log.e("uri", "null");
+                    flag = true;
+                } else {
+                    Log.e("uri", "null");
+                    flag = false;
+                }
             }
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = text_name.getText().toString().trim();
-                String pass = text_pass.getText().toString();
-
-                SharedPreferences.Editor editor = getSharedPreferences("lock", MODE_PRIVATE).edit();
-                editor.putString("name", name);
-                editor.putString("pass", pass);
-                editor.commit();
-            }
-        });
-
-        text_name = (EditText) findViewById(R.id.editText_name);
-        text_pass = (EditText) findViewById(R.id.editText_pass);
-
-        SharedPreferences read = getSharedPreferences("lock", MODE_PRIVATE);
-        text_name.setText(read.getString("name", "Samba address"));
-        text_pass.setText(read.getString("pass", "Password"));
     }
 
     @Override
@@ -84,5 +148,10 @@ public class ScrollingActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showMessage(String s) {
+        Snackbar.make(findViewById(R.id.fab), s, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
